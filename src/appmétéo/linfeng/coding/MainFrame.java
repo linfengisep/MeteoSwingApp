3e364d65fc7d5f2585e7d3b0fef8687a6b81eab3
linfengisep <wang.linfeng0213@gmail.com>
2019-03-24 12:20:38 +0100

    update asych okhttp request

--- src/appmétéo/linfeng/coding/MainFrame.java
+++ src/appmétéo/linfeng/coding/MainFrame.java
@@ -7,14 +7,15 @@ package appmétéo.linfeng.coding;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.SwingWorker;
 import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  *
@@ -27,52 +28,35 @@ public class MainFrame extends JFrame {
         String sources = "https://api.darksky.net/forecast/";
         double logitude = 37.8267;
         double latitude = -122.4233;
-        //String meteoUrl = String.format(Locale.FRANCE,"%s/forecast/%s/%.4f,%.4f",sources,apiKey,logitude,latitude);
-        //System.out.println("Url:"+meteoUrl);
         String meteoUrl = sources+apiKey+"/"+logitude+","+latitude;
 
         System.out.println("Avant le requête.");
-        new MeteoWorker(meteoUrl).execute();
-        System.out.println("Après le requête.");
-        
-    }
-    
-    class MeteoWorker extends SwingWorker<String,Void>{
-        private String meteoUrl;
-        public MeteoWorker(String url){
-            this.meteoUrl = url;
-        }
-
-       @Override
-            protected String doInBackground() throws Exception {
-               System.out.println("Current Tread in Swingworker:"+Thread.currentThread().getName());
-               OkHttpClient client = new OkHttpClient();
+          OkHttpClient client = new OkHttpClient();
                  Request request = new Request
                                     .Builder()
-                                    .url(this.meteoUrl)
+                                    .url(meteoUrl)
                                     .build();
-                 
-                Call call = client.newCall(request);
-                try{
-                    Response response = call.execute();
-                    //return "result";
-                    if(response.isSuccessful()){
-                        return response.body().string();
-                    }
-                }catch(IOException e){
-                    System.err.println(e);
-                }    
-                return null;
-            }
-            
-            @Override
-            protected void done(){
-                try {
-                    //super.done();
-                    System.out.println("Response: "+get());
-                } catch (InterruptedException | ExecutionException ex) {
-                    System.err.println(ex);
-                }
-            }
+                 client.newCall(request).enqueue(new Callback(){
+                   @Override
+                   public void onFailure(Call call, IOException ioe) {
+                       ioe.printStackTrace();
+                   }
+
+                   @Override
+                   public void onResponse(Call call, Response response) throws IOException { 
+                       System.out.println("Current thread: "+Thread.currentThread().getName());
+                       try(ResponseBody responseBody = response.body()){
+                           if(! response.isSuccessful()) throw new IOException("Unexpected code: "+response);
+                           
+                           Headers responseHeaders = response.headers();
+                           for(int i=0,size = responseHeaders.size();i<size;i++){
+                               //System.out.println(responseHeaders.name(i)+", "+responseHeaders.value(i));
+                           }
+                           System.out.println(responseBody.string());
+                       }
+                   }
+                 });
+        System.out.println("Après le requête.");
+
     }
 }
